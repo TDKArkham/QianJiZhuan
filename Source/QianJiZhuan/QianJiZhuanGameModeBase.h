25df1a754a3bb63915556bd9ba4049dabeb66a9f
TDKArkham <LCMTDK117@gmail.com>
2022-09-12 14:11:45 +0800

    Initialize Project
    - Import the Scene One map to project.

---
+++ Source/QianJiZhuan/QianJiZhuanGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QianJiZhuanGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QIANJIZHUAN_API AQianJiZhuanGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
